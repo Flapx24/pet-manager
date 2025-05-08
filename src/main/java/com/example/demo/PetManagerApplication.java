80b73b5b201a53b7a998bd732268e4f0338b3480
AntonioMoralesCaldero <antoniommoralescaldero@gmail.com>
2025-05-08 09:28:19 +0200

    Properties and thymeleaf solved, also index html  with css

--- src/main/java/com/example/demo/PetManagerApplication.java
+++ src/main/java/com/example/demo/PetManagerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetManagerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PetManagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PetManagerApplication.class, args);
+    }
 }
