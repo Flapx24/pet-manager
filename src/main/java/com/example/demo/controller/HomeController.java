065d1417a1615dedc39c82d2f7071fc18a2c4995
Álvaro <alvarocc948@gmail.com>
2025-05-13 11:11:01 +0200

    historyVaccine and data

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/vaccine")
+    public String vaccine(){
+        return "historyVaccine";
+    }
 }
